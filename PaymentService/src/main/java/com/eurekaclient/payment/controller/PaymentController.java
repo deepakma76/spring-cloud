278ae424cb5fe9d99dbf17bb6e58951c44965276
Deepak Mandal <deepakma76@gmail.com>
2023-08-19 22:30:37 +0530

    add spring cloud ribbon with eureka

--- PaymentService/src/main/java/com/eurekaclient/payment/controller/PaymentController.java
+++ PaymentService/src/main/java/com/eurekaclient/payment/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.eurekaclient.payment.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PaymentController {
 
+	@Value("${server.port}")
+	String port;
+
 	@PostMapping("/payment/{price}")
 	public String payment(@PathVariable Integer price) {
-		return price + " received successfully";
+		return price + " received successfully, server running on port " + port;
 	}
 }
